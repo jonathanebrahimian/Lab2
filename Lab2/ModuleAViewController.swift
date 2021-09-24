e746f129b75dbcf0b363ebb61fe9aa671f88bc35
JonyZizou <jonyzizou@gmail.com>
2021-09-24 00:31:54 -0500

    initial UI

---
+++ Lab2/ModuleAViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ModuleAViewController.swift
+//  Lab2
+//
+//  Created by Jonathan Ebrahimian on 9/24/21.
+//
+
+import UIKit
+
+class ModuleAViewController: UIViewController {
+
+    @IBOutlet weak var tone1Label: UILabel!
+    @IBOutlet weak var tone2Label: UILabel!
+    @IBOutlet weak var lockInButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func lockInClicked(_ sender: Any) {
+    }
+    
+
+
+}
---
+++ Lab2/ModuleBViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ModuleBViewController.swift
+//  Lab2
+//
+//  Created by Jonathan Ebrahimian on 9/24/21.
+//
+
+import UIKit
+
+class ModuleBViewController: UIViewController {
+
+    @IBOutlet weak var movementLabel: UILabel!
+    @IBOutlet weak var slider: UISlider!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func sliderChanged(_ sender: Any) {
+    }
+    
+
+
+}
