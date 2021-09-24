dfa31d5895a9f301f1a2d0614941387f10042249
JonyZizou <jonyzizou@gmail.com>
2021-09-24 00:21:30 -0500

    Initial Commit

---
+++ Lab2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Lab2
+//
+//  Created by Jonathan Ebrahimian on 9/24/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
