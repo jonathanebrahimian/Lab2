50f007c4416172b49b363e55e57aa4854e0031a7
Nathan Gage <ngage@smu.edu>
2021-10-01 12:40:11 -0500

    Format code

--- Lab2/AudioModel.swift
+++ Lab2/AudioModel.swift
@@ -30,45 +30,45 @@ class AudioModel {
 
     // MARK: Module B
     // baseline information for gesture detection
-    var rightMaxAvgBaseline:[Float]
-    var leftMaxAvgBaseline:[Float]
+    var rightMaxAvgBaseline: [Float]
+    var leftMaxAvgBaseline: [Float]
 
-    var leftBaseline:Float;
-    var rightBaseline:Float;
+    var leftBaseline: Float
+    var rightBaseline: Float
 
-    let numBaselineFrames = 5;
-    var peakAvg:[Float]
-    
+    let numBaselineFrames = 5
+    var peakAvg: [Float]
+
     // gesture detection thresholds
-    var rightPercentage:Float;
-    var leftPercentage:Float;
-    
+    var rightPercentage: Float
+    var leftPercentage: Float
+
     // ring buffer of previous maxes for gesture detection
-    var rightMaxAvg:[Float]
-    var leftMaxAvg:[Float]
-    
+    var rightMaxAvg: [Float]
+    var leftMaxAvg: [Float]
+
     // track position in ring buffer
-    var insertMaxIndex:Int; 
+    var insertMaxIndex: Int;
 
     // numer of frames to use for detecting average
-    let numDetectionFrames = 5;
+    let numDetectionFrames = 5
 
     // number of elements analyzed left & right of peak
-    let windowSize = 6; 
-    
+    let windowSize = 6;
+
     // status variables
-    var caputringBaselines:Bool;
-    var startDetection:Bool;
-    
+    var caputringBaselines: Bool
+    var startDetection: Bool
+
     //index of max value in fft data
-    var maxIndex:Int;
-    
+    var maxIndex: Int
+
     //gesture recognized
-    var gesture:String;
-    
+    var gesture: String
+
     // arrays to help debuging
-    var statsLeft:[Float] = [];
-    var statsRight:[Float] = [];
+    var statsLeft: [Float] = []
+    var statsRight: [Float] = []
 
     // MARK: Public Methods
     init(buffer_size: Int) {
@@ -76,14 +76,14 @@ class AudioModel {
 
         // anything not lazily instatntiated should be allocated here
         timeData = Array.init(repeating: 0.0, count: BUFFER_SIZE)
-        fftData = Array.init(repeating: 0.0, count: BUFFER_SIZE/2)
-        
+        fftData = Array.init(repeating: 0.0, count: BUFFER_SIZE / 2)
+
         //set all vars to step 0 values
         rightMaxAvgBaseline = Array()
         leftMaxAvgBaseline = Array()
         peakAvg = Array()
-        rightMaxAvg =  Array.init(repeating: 0.0, count: numDetectionFrames)
-        leftMaxAvg =  Array.init(repeating: 0.0, count: numDetectionFrames)
+        rightMaxAvg = Array.init(repeating: 0.0, count: numDetectionFrames)
+        leftMaxAvg = Array.init(repeating: 0.0, count: numDetectionFrames)
         rightBaseline = 0
         leftBaseline = 0
         caputringBaselines = true //start capturing baselines
@@ -97,10 +97,10 @@ class AudioModel {
 
     func startProcessingSinewaveForPlayback(withFreq: Float = 330.0) {
         sineFrequency = withFreq
-        
+
         //get index of the peak in the fft (from equation)
-        maxIndex = Int((Float(sineFrequency) * Float(BUFFER_SIZE ) / Float(Novocaine.audioManager().samplingRate)).rounded());
-        
+        maxIndex = Int((Float(sineFrequency) * Float(BUFFER_SIZE) / Float(Novocaine.audioManager().samplingRate)).rounded())
+
         self.audioManager?.setOutputBlockToPlaySineWave(sineFrequency)
     }
 
@@ -139,7 +139,7 @@ class AudioModel {
                                  selector: #selector(self.runEveryInterval),
                                  userInfo: nil,
                                  repeats: true)
-         
+
         }
     }
 
@@ -151,46 +151,46 @@ class AudioModel {
         return CircularBuffer.init(numChannels: Int64(self.audioManager!.numInputChannels),
                                    andBufferSize: Int64(BUFFER_SIZE))
     }()
-    
+
     /* Get the average of the inputted float array.
      * This code was adapted from the stack overflow link below:
      * https://stackoverflow.com/questions/43703823/how-to-find-the-average-value-of-an-array-swift
      */
-    func getAvg(arr: [Float]) -> Float{
-        let sumArray = arr.reduce(0, +);
-        return sumArray / Float(arr.count);
+    func getAvg(arr: [Float]) -> Float {
+        let sumArray = arr.reduce(0, +)
+        return sumArray / Float(arr.count)
     }
-    
+
     /* Get the max values to the left and right of the peak.
      * The range is = to window size.
      * We skiped the element dirrectly to the left and right of the peak for better results
      */
-    func getMaxes() -> (leftMax: Float,rightMax: Float) {
+    func getMaxes() -> (leftMax: Float, rightMax: Float) {
         //checking for index out of bounds
-        let leftBottom = max(maxIndex-windowSize-2,0);
-        let leftTop = max(maxIndex-2,0);
-        let rightTop = min(maxIndex+windowSize+2,fftData.count-1);
-        let rightBottom = min(maxIndex+2,fftData.count-1);
-        
+        let leftBottom = max(maxIndex - windowSize-2, 0)
+        let leftTop = max(maxIndex - 2, 0)
+        let rightTop = min(maxIndex + windowSize + 2, fftData.count - 1)
+        let rightBottom = min(maxIndex + 2, fftData.count - 1)
+
         //get max to left and right
-        let leftMax = fftData[leftBottom...leftTop].max();
-        let rightMax = fftData[rightBottom...rightTop].max();
-        return (leftMax!, rightMax!);
+        let leftMax = fftData[leftBottom...leftTop].max()
+        let rightMax = fftData[rightBottom...rightTop].max()
+        return (leftMax!, rightMax!)
     }
-    
+
     /* Params: Float
      *
      * This method updates the sine wave with the inputed float.
      * Also resets all calculation values to their base state.
      * This will make us recalculate baselines.
      */
-    func changeFrequency(frequencyIn:Float) {
+    func changeFrequency(frequencyIn: Float) {
         //remove all old data
-        rightMaxAvgBaseline.removeAll();
-        leftMaxAvgBaseline.removeAll();
+        rightMaxAvgBaseline.removeAll()
+        leftMaxAvgBaseline.removeAll()
         peakAvg.removeAll()
-        rightMaxAvg =  Array.init(repeating: 0.0, count: numDetectionFrames)
-        leftMaxAvg =  Array.init(repeating: 0.0, count: numDetectionFrames)
+        rightMaxAvg = Array.init(repeating: 0.0, count: numDetectionFrames)
+        leftMaxAvg = Array.init(repeating: 0.0, count: numDetectionFrames)
         rightBaseline = 0
         leftBaseline = 0
         caputringBaselines = true
@@ -199,60 +199,60 @@ class AudioModel {
         rightPercentage = 0
         leftPercentage = 0
         gesture = "neutral"
-        
+
         //set new frequency
         sineFrequency = frequencyIn
-        
+
         //compute new peak index
-        maxIndex = Int((Float(sineFrequency) * Float(BUFFER_SIZE ) / Float(Novocaine.audioManager().samplingRate)).rounded());
+        maxIndex = Int((Float(sineFrequency) * Float(BUFFER_SIZE) / Float(Novocaine.audioManager().samplingRate)).rounded())
     }
-    
+
     /* This method sets the baselines for the current frequency playing as well
      * as the threshold for motion detection.
      */
     func captureBaselines() {
-        
+
         //get maxes
-        let maxes = getMaxes();
-        
+        let maxes = getMaxes()
+
         //store max for current frame
-        rightMaxAvgBaseline.append(maxes.rightMax);
-        leftMaxAvgBaseline.append(maxes.leftMax);
-        
+        rightMaxAvgBaseline.append(maxes.rightMax)
+        leftMaxAvgBaseline.append(maxes.leftMax)
+
         //store peak
         peakAvg.append(fftData[maxIndex])
 
         //if numBaselineFrames captured, stop taking baselines
-        
-        if(rightMaxAvgBaseline.count == numBaselineFrames && leftMaxAvgBaseline.count == numBaselineFrames){
+
+        if(rightMaxAvgBaseline.count == numBaselineFrames && leftMaxAvgBaseline.count == numBaselineFrames) {
             //get average of the maxes over numBaselineFrames and store them in the respective baseline variables
-            rightBaseline = getAvg(arr:rightMaxAvgBaseline);
-            leftBaseline = getAvg(arr:leftMaxAvgBaseline);
-            
+            rightBaseline = getAvg(arr: rightMaxAvgBaseline)
+            leftBaseline = getAvg(arr: leftMaxAvgBaseline)
+
             //get average peak over numBaselineFrames (used in threshold calculation)
             let peakBaseline = getAvg(arr: peakAvg)
-            
+
             //stop capturing baselines (start detecting motion)
             self.caputringBaselines = false
             print("Right Baseline:")
             print(rightBaseline)
             print("Left Baseline:")
             print(leftBaseline)
-            
+
             //set thresholds
             //from brute force testing we found good percentages for 20k
             //we then noticed that we needed to exponentioally decrease these percentages the lower the frequency
             //we decrease our "baseline percentage for 20k" by .00025 * (20000/sineFreqncy) ^ 2
-            leftPercentage = (peakBaseline - leftBaseline) * (0.2003 - (0.00025 * pow((20000/sineFrequency),2)))
-            rightPercentage = (peakBaseline - rightBaseline) * (0.2525 - (0.00025 * pow((20000/sineFrequency),2)))
+            leftPercentage = (peakBaseline - leftBaseline) * (0.2003 - (0.00025 * pow((20000 / sineFrequency), 2)))
+            rightPercentage = (peakBaseline - rightBaseline) * (0.2525 - (0.00025 * pow((20000 / sineFrequency), 2)))
             print("Left percentage:")
             print(leftPercentage)
             print("Right percentage:")
             print(rightPercentage)
         }
-        
+
     }
-    
+
     /* This method will detect motion.
      * We decided to take an average of the maxes over numDetectionFrames number of frames.
      * We wait numDetectionFrames till we have arrays full of maxes. Each time we get a new fft,
@@ -262,48 +262,48 @@ class AudioModel {
      */
     func detectMotion() {
         //get maxes
-        let maxes = getMaxes();
-        
+        let maxes = getMaxes()
+
         //store max from current frame in insertMaxIndex location
-        rightMaxAvg[insertMaxIndex] = maxes.rightMax;
-        leftMaxAvg[insertMaxIndex] = maxes.leftMax;
+        rightMaxAvg[insertMaxIndex] = maxes.rightMax
+        leftMaxAvg[insertMaxIndex] = maxes.leftMax
         insertMaxIndex += 1 //go to next location
-        
+
         //if our index has reached numDetectionFrames (it is out of bounds) reset it to 0 (this will start the overwriting process)
-        if(insertMaxIndex == numDetectionFrames){
+        if(insertMaxIndex == numDetectionFrames) {
             insertMaxIndex = 0
             startDetection = true //start detection is initialzed as false so we will only start detecting once we have a full arr of maxes
         }
-        
-        if(startDetection){
+
+        if(startDetection) {
             //detection algorithm
-            
+
             //get the average of the maxes
-            let rightAvg = getAvg(arr:rightMaxAvg);
-            let leftAvg = getAvg(arr:leftMaxAvg);
-            
+            let rightAvg = getAvg(arr: rightMaxAvg)
+            let leftAvg = getAvg(arr: leftMaxAvg)
+
 //          used for debugging
 //            statsLeft.append(leftAvg - leftBaseline);
 //            statsRight.append(rightAvg - rightBaseline);
-            
+
             //detect if the difference between our current avg and baseline exceeds threshold
-            if(rightAvg - rightBaseline > rightPercentage){
+            if(rightAvg - rightBaseline > rightPercentage) {
                 print("towards")
                 gesture = "towards"
-            }else if(leftAvg - leftBaseline > leftPercentage){
+            } else if(leftAvg - leftBaseline > leftPercentage) {
                 print("away")
                 gesture = "away"
-            }else{
+            } else {
                 gesture = "neutral"
             }
-            
+
         }
     }
-    
+
     @objc
     private func runEveryInterval() {
         if inputBuffer != nil {
-           
+
             // copy time data to swift array
             self.inputBuffer!.fetchFreshData(&timeData,
                                              withNumSamples: Int64(BUFFER_SIZE))
@@ -349,7 +349,7 @@ class AudioModel {
                 }
                 // if we have multiple valid peaks
                 else if peakIndexes.count > 1 {
-                    
+
                     // for finding largest 2
                     var largestA: Float = -MAXFLOAT
                     var largestAIndex: Int = -1
@@ -381,16 +381,16 @@ class AudioModel {
                 }
 
                 updateLabels!(firstPeak, secondPeak)
-            }            
+            }
             else if currentModule == "b" {
                 //switch between capturing baselines and detecting motion
-                if(caputringBaselines){
+                if(caputringBaselines) {
                     captureBaselines()
-                }else{
+                } else {
                     detectMotion()
                 }
             }
-            
+
         }
     }
 
@@ -398,7 +398,7 @@ class AudioModel {
         // copy samples from the microphone into circular buffer
         self.inputBuffer?.addNewFloatData(data, withNumSamples: Int64(numFrames))
     }
-    
+
     //==========================================
     // MARK: Private Methods
 
--- Lab2/ModuleBViewController.swift
+++ Lab2/ModuleBViewController.swift
@@ -9,52 +9,48 @@ import UIKit
 import Metal
 
 class ModuleBViewController: UIViewController {
-    struct AudioConstants{
-        static let AUDIO_BUFFER_SIZE = 1024*4
+    struct AudioConstants {
+        static let AUDIO_BUFFER_SIZE = 1024 * 4
     }
-    
+
     // setup audio model
 
     let audio = AudioModel(buffer_size: AudioConstants.AUDIO_BUFFER_SIZE)
-    lazy var graph:MetalGraph? = {
+    lazy var graph: MetalGraph? = {
         return MetalGraph(mainView: self.view)
     }()
     @IBOutlet weak var frequencySlider: UISlider!
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // start up the audio model here, querying microphone
-        
+
         audio.startMicrophoneProcessing(withFps: 10)
         //start at freq 20k
         audio.startProcessingSinewaveForPlayback(withFreq: 20000)
         audio.play(forModule: "b")
         frequencySlider.value = 20000 //start at freq 20k
-        
-        
-        
-        // Do any additional setup after loading the view.
     }
-    
+
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
-        
+
         audio.play(forModule: "b")
-        
+
         graph?.addGraph(withName: "fft",
                         shouldNormalize: true,
-                        numPointsInGraph: AudioConstants.AUDIO_BUFFER_SIZE/2)
-        
+                        numPointsInGraph: AudioConstants.AUDIO_BUFFER_SIZE / 2)
+
         graph?.addGraph(withName: "time",
-            shouldNormalize: false,
-            numPointsInGraph: AudioConstants.AUDIO_BUFFER_SIZE)
-        
+                        shouldNormalize: false,
+                        numPointsInGraph: AudioConstants.AUDIO_BUFFER_SIZE)
+
 
         Timer.scheduledTimer(timeInterval: 0.05, target: self,
-            selector: #selector(self.updateGraph),
-            userInfo: nil,
-            repeats: true)
-       
+                             selector: #selector(self.updateGraph),
+                             userInfo: nil,
+                             repeats: true)
+
     }
 //
     override func viewWillDisappear(_ animated: Bool) {
@@ -62,40 +58,40 @@ class ModuleBViewController: UIViewController {
         super.viewWillDisappear(animated)
         audio.pause()
     }
-    
-    
-   
- 
+
+
+
+
     @IBOutlet weak var freqLabel: UILabel!
-    
+
 
     @IBOutlet weak var gestureType: UILabel!
-    
-    
+
+
     @IBAction func changeFrequency(_ sender: UISlider) {
         //change frequency when slider is updated
         self.audio.changeFrequency(frequencyIn: sender.value)
         freqLabel.text = "Frequency: \(sender.value)"
     }
-    
-    
+
+
     @objc
-    func updateGraph(){
+    func updateGraph() {
         self.graph?.updateGraph(
             data: self.audio.fftData,
             forKey: "fft"
         )
-        
+
         self.graph?.updateGraph(
             data: self.audio.timeData,
             forKey: "time"
         )
-        
+
         //every time we update the graph, update the gesture label
         DispatchQueue.main.async {
             self.gestureType.text = self.audio.gesture
         }
-   
+
     }
-    
+
 }
